84468567001381e64f3967f980c0f1afbf70a2f4
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-06-20 17:15:08 +0100

    add initial files and data

--- README.md
+++ README.md
@@ -1,2 +1,118 @@
-# zendesk-scripts
-Various scripts in various languages to interact with GDS Zendesk
+# Zendesk Scripts
+
+Series of tasks to locate and remove legacy zendesk tickets and users to meet GDPR
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
+
+Clone the repo
+
+```
+git clone git@github.com:alphagov/zendesk-scripts
+```
+
+
+### Prerequisites
+
+Use of linux 'screen' command is highly recommended
+
+Use dry run for testing
+
+```
+export DRY_RUN=true
+```
+
+ruby setup
+
+```
+bundle exec rake -T 
+```
+
+Add environmental variables to .bashrc
+```
+# Vars for Zendesk
+export ZENDESK_USER_PASSWORD=[zendesk admin-user password]
+export ZENDESK_USER_EMAIL=[zendesk admin-user email address]
+export ZENDESK_URL=https://govuk.zendesk.com/api/v2
+```
+
+
+## Testing
+
+Set the DRY_RUN variable and execute the scripts as per live process.
+
+
+## Process
+
+Start a 'screen' session, e.g.
+
+```
+screen -S "delete tickets 2013"
+```
+
+### Tickets
+* Extract local files in /data directory per year of ticket_id's
+
+```
+bundle exec ruby lib/get-annual-ticket-numbers.rb
+```
+
+####   Results
+
+Files are created per year, e.g.
+
+```data/y2013_tickets```
+
+
+* Execute script per year to delete old tickets (uses above files as input)
+
+```
+data/delete_tickets_2013.sh
+```
+
+* Exit the session but leave the script running
+```
+ ctrl+a
+ d
+```
+
+* To resume the session
+
+```
+screen -r [sessionname]
+```
+
+
+
+Notes
+* The scripts should be run inside a screen session
+* Deletion takes several hours (for e.g. 50,000 tickets) depending on quantities and no. of scripts executing in parallel
+* Check the API limitations, currently 700 requests / minute
+
+
+### Users
+* Execute ruby script to remove selected users
+
+tbd ???
+
+
+
+## Contributing
+
+Please read [???](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+
+
+## Authors
+
+* **Issy Long** - Initial ruby work
+* **David Pye** - Refactor task into year chunks and some bashing
+
+
+## License
+
+This project is licensed under the MIT License
+
+## Acknowledgments
+
---
+++ lib/zendesk_cleaner.rb
@@ -0,0 +1,99 @@
+require 'set'
+require 'time'
+
+class ZendeskCleaner
+  attr_reader :client, :users_with_existing_tickets, :tickets_to_delete, :users_to_delete
+
+  def initialize(client, dry_run, groups = [])
+    @client = client
+    @dry_run = dry_run
+    @groups = groups
+    @users_with_existing_tickets = Set.new
+    @tickets_to_delete = Set.new
+    @users_to_delete = Set.new
+  end
+
+  def display_current_num_of_tickets_and_users
+    puts "This Zendesk instance has #{@client.users.count} users and #{@client.tickets.count} tickets."
+  end
+
+  def how_many_things_have_we_deleted
+    puts "We have deleted #{@users_to_delete.size} users and #{@tickets_to_delete.size} tickets."
+  end
+
+  def delete_old_tickets
+    @client.tickets.all! do |ticket|
+      users_with_existing_tickets.add(ticket.requester_id) if client.users.find!(id: ticket.requester_id)
+
+      next unless @groups.include?(ticket.group_id)
+
+      delete_tickets_older_than_a_year(ticket)
+    end
+  end
+
+  # As we've already deleted tickets which have not been updated
+  # for a year (`tickets_for_deletion`), their associations to users
+  # should be gone too, meaning that `users_with_existing_tickets` will
+  # only be the ones who have have been active within a year.
+  def delete_old_users
+    @client.users.all! do |user|
+      if is_end_user?(user) &&
+          is_not_in_an_org?(user) &&
+          is_not_no_reply?(user) &&
+          @users_with_existing_tickets.exclude?(user.id) &&
+          has_been_active_within_a_year?(user)
+
+        @users_to_delete.add(user.id)
+        if @dry_run
+          puts user
+          print '.'
+        else
+          user.destroy!
+        end
+      end
+    end
+  end
+
+private
+
+  def has_been_active_within_a_year?(user)
+    if user.last_login_at
+      return user.last_login_at < 1.year.ago.to_datetime
+    end
+
+    latest_update = user.updated_at || user.created_at
+    latest_update < 1.year.ago.to_datetime
+  end
+
+  def delete_tickets_older_than_a_year(ticket)
+    if is_closed?(ticket) && not_updated_for_a_year?(ticket)
+      @tickets_to_delete.add(ticket.id)
+      if @dry_run
+        print '/'
+      else
+        ticket.destroy!
+      end
+    end
+  end
+
+  def is_closed?(ticket)
+    ticket.status == 'closed'
+  end
+
+  def not_updated_for_a_year?(ticket)
+    latest_update = ticket.updated_at || ticket.created_at
+    latest_update < "2013-01-01T00:00:00+00:00".to_datetime
+  end
+
+  def is_end_user?(user)
+    user.role.name == "end-user"
+  end
+
+  def is_not_in_an_org?(user)
+    user.organization_id.nil?
+  end
+
+  def is_not_no_reply?(user)
+    user.name != "Zendesk"
+  end
+end
