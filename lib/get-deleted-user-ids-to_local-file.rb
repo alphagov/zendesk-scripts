0885e0dd3240a7b482f1eed94084734722222b6c
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-09-05 17:38:32 +0100

    change output file structure to real json

--- lib/get-deleted-user-ids-to_local-file.rb
+++ lib/get-deleted-user-ids-to_local-file.rb
@@ -12,31 +12,22 @@ require 'json'
   config.retry = true
 end
 
-# day = Date.today.prev_day
-# lastyear = day - 365
-# puts "last year:#{lastyear}"
 
-# begin
-#   search_results = RestClient::Request.execute method: :get, url: "#{ENV['ZENDESK_URL']}/deleted_users.json", user: ENV['ZENDESK_USER_EMAIL'], password: ENV['ZENDESK_USER_PASSWORD']
-# rescue RestClient::ExceptionWithResponse => err
-#   err.response
-# end
-
-# TBC NEED TO LIMIT THE DATE RANGE AS 8000+ PAGES CURRENTLY!!!
+# TBC CAN WE LIMIT THE DATE RANGE AS 8000+ PAGES CURRENTLY!!!
 
 begin
   url = "https://govuk.zendesk.com/api/v2/deleted_users.json"
   output_results = []
-  while url do
+  while url != "https://govuk.zendesk.com/api/v2/deleted_users.json?page=5"
     search_results = JSON.parse(RestClient::Request.execute method: :get, url: url, user: ENV['ZENDESK_USER_EMAIL'], password: ENV['ZENDESK_USER_PASSWORD'])
-    output_results << search_results
+    (output_results << search_results['deleted_users']).flatten!
     puts search_results['next_page']
     # puts "output_results: #{output_results}"
     url = search_results['next_page']
   end
 end
 
-File.open("data/soft_deleted_users", "w") { |file| file.write(output_results) }
+File.open("data/soft_deleted_users.page1-4", "w") { |file| file.write(JSON.generate(output_results)) }
 
 
 
