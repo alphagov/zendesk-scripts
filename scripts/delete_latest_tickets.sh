103f039a6150f44fea80344335c6ab4404018dc1
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-09-12 11:05:35 +0100

    Fix filename in get and delete scripts

---
+++ scripts/delete_latest_tickets.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+today=`date -I`
+source_file="data/latest-tickets-to-purge"
+echo "source: ${source_file}"
+
+
+if [ "${DRY_RUN+1}" ] ; then
+  for raw_id in `cat ${source_file}`
+		do
+			clean_id=`echo $raw_id | sed -e 's/\,//g' | sed -e 's/\]//g' | sed -e 's/\[//g'`
+			echo "*** DRY_RUN TICKET ID = $clean_id ***"
+			echo "curl $ZENDESK_URL/tickets/$clean_id -v -u $ZENDESK_USER_EMAIL:ZENDESK_USER_PASSWORD -X DELETE"
+		done
+
+else
+
+	for raw_id in `cat data/${source_file}`
+		do
+			clean_id=`echo $raw_id | sed -e 's/\,//g' | sed -e 's/\]//g' | sed -e 's/\[//g'`
+			echo "REALLY DELETE"
+			curl $ZENDESK_URL/tickets/$clean_id -v -u $ZENDESK_USER_EMAIL:$ZENDESK_USER_PASSWORD -X DELETE
+		done
+fi
