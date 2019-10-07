5a99f6728460a046d6f1d923069d2eed2b89aa55
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-10-07 15:18:08 +0100

    Ammend the file open method to suit reviewer comments.
    This is more rubyesque in it's structure.

--- lib/delete-users-using-local-file.rb
+++ lib/delete-users-using-local-file.rb
@@ -11,98 +11,99 @@ diag = "true"
 url = "#{ENV['ZENDESK_URL']}/deleted_users/"
 lastyear = Date.today.next_day - 365
 
-input_file = File.open "data/soft_deleted_users.json"
+File.open "data/soft_deleted_users.json" do |input_file|
 
-file_data = JSON.load input_file
+  file_data = JSON.load input_file
 
-file_data.each do |user|
+  file_data.each do |user|
 
 
-  # Extract some fields
-  updated_at = user["updated_at"]
-  last_login_at = user["last_login_at"]
-  user_id = user["id"]
-  active = user["active"]
-  name = user["name"]
+    # Extract some fields
+    updated_at = user["updated_at"]
+    last_login_at = user["last_login_at"]
+    user_id = user["id"]
+    active = user["active"]
+    name = user["name"]
 
-  # Convert strings to dates, if strings are nil then set a false date of 2012 so comparison works.
-  # Booleans returned by API are not true boolean but strings.
+    # Convert strings to dates, if strings are nil then set a false date of 2012 so comparison works.
+    # Booleans returned by API are not true boolean but strings.
 
-  if active == "false"
-    # account is already soft deleted so force hard delete
-    hard_delete = "true"
-  end
-
-  false_date = "2012-01-01"
-
-  if hard_delete != "true"
-    if updated_at.nil?
-      updated_at = false_date
+    if active == "false"
+      # account is already soft deleted so force hard delete
+      hard_delete = "true"
     end
 
-    # parse the date so we can do comparisons
+    false_date = "2012-01-01"
 
-    updated = Date.parse(updated_at)
+    if hard_delete != "true"
+      if updated_at.nil?
+        updated_at = false_date
+      end
 
-    if last_login_at != nil
-      last_login = Date.parse(last_login_at)
+      # parse the date so we can do comparisons
+
+      updated = Date.parse(updated_at)
+
+      if last_login_at != nil
+        last_login = Date.parse(last_login_at)
+      else
+        last_login_at = false_date
+        last_login = Date.parse(last_login_at)
+      end
+
+      # diagnostics 
+
+      if diag == "true"
+        puts "user_id: #{user_id}"
+        puts "Name: #{name}"
+        puts "Active: #{active}"
+        puts "last_login_at: #{last_login_at}"
+        puts "last_login: #{last_login}"
+        puts "url: #{url}"
+        puts "lastyear: #{lastyear}"
+      end
+
+    # If last logged in < last year, let's check whether user has any tickets associated
+
+      if last_login <= lastyear
+        puts "Potential DELETION candidate - check tickets"
+        count = @client.search!(:query => "requester_id:#{user_id}").count
+        ticket_count = Integer count
+        puts "ticket_count: #{ticket_count}"
+      else
+        puts "DO NOT DELETE"
+      end
     else
-      last_login_at = false_date
-      last_login = Date.parse(last_login_at)
+    ticket_count = 0
     end
+    
+    if ticket_count == 0
+      # soft delete
+      puts "ok we're ready, DELETE this user: #{user_id}"
+      puts "DELETE User ID: #{user_id}"
 
-    # diagnostics 
+      begin
+        @client.users.destroy!(:id => user_id)
+        rescue ZendeskAPI::Error::RecordInvalid => api_error
+          puts "Received error user #{user_id} already deleted"
+          puts "Skipping over user #{user_id}"
+        next
+      end
 
-    if diag == "true"
-      puts "user_id: #{user_id}"
-      puts "Name: #{name}"
-      puts "Active: #{active}"
-      puts "last_login_at: #{last_login_at}"
-      puts "last_login: #{last_login}"
-      puts "url: #{url}"
-      puts "lastyear: #{lastyear}"
-    end
+      begin
+      # api does not support hard delete yet, so hard delete like this...
+        full_url = "#{url}#{user_id}.json"
+        RestClient::Request.execute method: :delete, url: full_url, user: ENV['ZENDESK_USER_EMAIL'], password: ENV['ZENDESK_USER_PASSWORD']
 
-  # If last logged in < last year, let's check whether user has any tickets associated
-
-    if last_login <= lastyear
-      puts "Potential DELETION candidate - check tickets"
-      count = @client.search!(:query => "requester_id:#{user_id}").count
-      ticket_count = Integer count
-      puts "ticket_count: #{ticket_count}"
+        rescue RestClient::Exception => api_error
+          puts "Received error from ZenDesk API for user #{user_id} => #{api_error}"
+          puts api_error.backtrace
+          puts "Skipping over user #{user_id}"
+        next
+      end  
     else
-      puts "DO NOT DELETE"
+      puts "NOT DELETING #{user_id}"
     end
-  else
-  ticket_count = 0
+    puts "---------------------------------"
   end
-  
-  if ticket_count == 0
-    # soft delete
-    puts "ok we're ready, DELETE this user: #{user_id}"
-    puts "DELETE User ID: #{user_id}"
-
-    begin
-      @client.users.destroy!(:id => user_id)
-      rescue ZendeskAPI::Error::RecordInvalid => api_error
-        puts "Received error user #{user_id} already deleted"
-        puts "Skipping over user #{user_id}"
-      next
-    end
-
-    begin
-    # api does not support hard delete yet, so hard delete like this...
-      full_url = "#{url}#{user_id}.json"
-      RestClient::Request.execute method: :delete, url: full_url, user: ENV['ZENDESK_USER_EMAIL'], password: ENV['ZENDESK_USER_PASSWORD']
-
-      rescue RestClient::Exception => api_error
-        puts "Received error from ZenDesk API for user #{user_id} => #{api_error}"
-        puts api_error.backtrace
-        puts "Skipping over user #{user_id}"
-      next
-    end  
-  else
-    puts "NOT DELETING #{user_id}"
-  end
-  puts "---------------------------------"
 end
