f122c7010e89550dc9e1ff5ecff99fef010643dc
David Pye <david.pye@digital.cabinet-office.gov.uk>
2020-01-21 16:43:23 +0000

    add agent role type and description

--- scripts/merge-agent-and-group-description.sh
+++ scripts/merge-agent-and-group-description.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# Requires
+# data/groups.out
+# data/custom-roles.out
+
 agents_in="data/agents.csv"
-groups_in="data/groups.json"
+custom_roles="data/custom-roles.out"
+groups_in="data/groups.out"
 agents_and_groups="data/agents-and-groups.csv"
-mv ${agents_and_groups} ${agents_and_groups}.bak
+
+if [ -f ${agents_and_groups} ]
+  then mv ${agents_and_groups} ${agents_and_groups}.bak
+fi
+
+echo "Name,Role,Default_Group,Active,Custom_Role_ID,Group_Description,Custom_Role_Description" > ${agents_and_groups}
 
 while IFS= read -r agent
 do
-  # echo "agent=${agent}"
   group_id=`echo ${agent} | cut -d "," -f3`
-  # echo "group_id=${group_id}"
   group_txt=`grep ${group_id} ${groups_in} | cut -d "," -f2`
-  # echo "group_txt=${group_txt}"
-  echo ${agent},${group_txt} >> ${agents_and_groups}
-  sort -t "," -k2  ${agents_and_groups} > ${agents_and_groups}.sorted
+  custom_role_id=`echo ${agent} | cut -d "," -f5`
+  custom_role_text=`grep ${custom_role_id} ${custom_roles} | cut -d "," -f2`
+  echo ${agent},${group_txt},${custom_role_text} >> ${agents_and_groups}
 
 done < ${agents_in}
